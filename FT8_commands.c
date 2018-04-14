6760e7f65fcb74954c4f6b3869c6123f141f6795
RudolphRiedel <31180093+RudolphRiedel@users.noreply.github.com>
2018-04-14 23:11:26 +0200

    Add files via upload
    - Added patching of the touch-interface for GT911 to FT8_init() if required by the module attached
    - simplified FT8_init() a bit
    - some house-keeping, fixing typos in comments left and right

--- FT8_commands.c
+++ FT8_commands.c
@@ -1,8 +1,8 @@
 /*
 @file    FT8_commands.c
 @brief   Contains Functions for using the FT8xx
-@version 3.8
-@date    2018-03-04
+@version 3.9
+@date    2018-04-14
 @author  Rudolph Riedel
 
 This file needs to be renamed to FT8_command.cpp for use with Arduino. 
@@ -87,13 +87,18 @@ This file needs to be renamed to FT8_command.cpp for use with Arduino.
 3.8
 - Added setting of REG_CSPRED to FT8_init() as new parameter, some Matrix Orbital modules use '0' instead of the reset-default '1'.
 
+3.9
+- Added patching of the touch-interface for GT911 to FT8_init() if required by the module attached
+- simplified FT8_init() a bit
+- some house-keeping, fixing typos in comments left and right
+
 */
 
 #include "FT8.h"
 #include "FT8_config.h"
 
 
-/* FT8xx Memory Commands - use with FT8_memWritexx and FT8_memReadxx */
+/* FT8xx Memory Commands - used with FT8_memWritexx and FT8_memReadxx */
 #define MEM_WRITE	0x80	/* FT8xx Host Memory Write */
 #define MEM_READ	0x00	/* FT8xx Host Memory Read */
 
@@ -114,13 +119,13 @@ void FT8_cmdWrite(uint8_t data)
 
 uint8_t FT8_memRead8(uint32_t ftAddress)
 {
-	uint8_t ftData8 = 0;
+	uint8_t ftData8;
 	FT8_cs_set();
-	spi_transmit((uint8_t)(ftAddress >> 16) | MEM_READ);	/* send Memory Write plus high address byte */
-	spi_transmit((uint8_t)(ftAddress >> 8));	/* send middle address byte */
+	spi_transmit((uint8_t)(ftAddress >> 16) | MEM_READ); /* send Memory Write plus high address byte */
+	spi_transmit((uint8_t)(ftAddress >> 8)); /* send middle address byte */
 	spi_transmit((uint8_t)(ftAddress));	/* send low address byte */
 	spi_transmit(0x00);	/* send dummy byte */
-	ftData8 = spi_receive(ftData8);	/* read data byte */
+	ftData8 = spi_receive(0x00); /* read data byte by sending another dummy byte */
 	FT8_cs_clear();
 	return ftData8;	/* return byte read */
 }
@@ -130,14 +135,14 @@ uint16_t FT8_memRead16(uint32_t ftAddress)
 {
 	uint16_t ftData16 = 0;
 	FT8_cs_set();
-	spi_transmit((uint8_t)(ftAddress >> 16) | MEM_READ);	/* send Memory Write plus high address byte */
-	spi_transmit((uint8_t)(ftAddress >> 8));	/* send middle address byte */
-	spi_transmit((uint8_t)(ftAddress));	/* send low address byte */
+	spi_transmit((uint8_t)(ftAddress >> 16) | MEM_READ); /* send Memory Write plus high address byte */
+	spi_transmit((uint8_t)(ftAddress >> 8)); /* send middle address byte */
+	spi_transmit((uint8_t)(ftAddress)); /* send low address byte */
 	spi_transmit(0x00);	/* send dummy byte */
 	ftData16 = (spi_receive(0x00));	/* read low byte */
 	ftData16 = (spi_receive(0x00) << 8) | ftData16;	/* read high byte */
 	FT8_cs_clear();
-	return ftData16;	/* return integer read */
+	return ftData16; /* return integer read */
 }
 
 
@@ -145,16 +150,16 @@ uint32_t FT8_memRead32(uint32_t ftAddress)
 {
 	uint32_t ftData32= 0;
 	FT8_cs_set();
-	spi_transmit((uint8_t)(ftAddress >> 16) | MEM_READ);	/* send Memory Write plus high address byte */
-	spi_transmit((uint8_t)(ftAddress >> 8));	/* send middle address byte */
+	spi_transmit((uint8_t)(ftAddress >> 16) | MEM_READ); /* send Memory Write plus high address byte */
+	spi_transmit((uint8_t)(ftAddress >> 8)); /* send middle address byte */
 	spi_transmit((uint8_t)(ftAddress));	/* send low address byte */
 	spi_transmit(0x00);	/* send dummy byte */
-	ftData32 = ((uint32_t)spi_receive(0x00));		/* read low byte */
+	ftData32 = ((uint32_t)spi_receive(0x00)); /* read low byte */
 	ftData32 = ((uint32_t)spi_receive(0x00) << 8) | ftData32;
 	ftData32 = ((uint32_t)spi_receive(0x00) << 16) | ftData32;
 	ftData32 = ((uint32_t)spi_receive(0x00) << 24) | ftData32; /* read high byte */
 	FT8_cs_clear();
-	return ftData32;	/* return long read */
+	return ftData32; /* return long read */
 }
 
 
@@ -173,10 +178,10 @@ void FT8_memWrite16(uint32_t ftAddress, uint16_t ftData16)
 {
 	FT8_cs_set();
 	spi_transmit((uint8_t)(ftAddress >> 16) | MEM_WRITE); /* send Memory Write plus high address byte */
-	spi_transmit((uint8_t)(ftAddress >> 8));	/* send middle address byte */
-	spi_transmit((uint8_t)(ftAddress));		/* send low address byte */
-	spi_transmit((uint8_t)(ftData16));		/* send data low byte */
-	spi_transmit((uint8_t)(ftData16 >> 8));	/* send data high byte */
+	spi_transmit((uint8_t)(ftAddress >> 8)); /* send middle address byte */
+	spi_transmit((uint8_t)(ftAddress)); /* send low address byte */
+	spi_transmit((uint8_t)(ftData16)); /* send data low byte */
+	spi_transmit((uint8_t)(ftData16 >> 8));  /* send data high byte */
 	FT8_cs_clear();
 }
 
@@ -244,7 +249,7 @@ uint32_t FT8_get_touch_tag(void)
 }
 
 
-/* order the command co-prozessor to start processing its FIFO que and do not wait for completion */
+/* order the command co-processor to start processing its FIFO queue and do not wait for completion */
 void FT8_cmd_start(void)
 {
 	uint32_t ftAddress;
@@ -261,7 +266,7 @@ void FT8_cmd_start(void)
 }
 
 
-/* order the command co-prozessor to start processing its FIFO que and wait for completion */
+/* order the command co-processor to start processing its FIFO queue and wait for completion */
 void FT8_cmd_execute(void)
 {
 	FT8_cmd_start();
@@ -272,6 +277,13 @@ void FT8_cmd_execute(void)
 void FT8_get_cmdoffset(void)
 {
 	cmdOffset = FT8_memRead16(REG_CMD_WRITE);
+}
+
+
+/* make current value of cmdOffset available while limiting access to that var to the FT8_commands module */
+uint16_t FT8_report_cmdoffset(void)
+{
+	return (cmdOffset);
 }
 
 
@@ -308,7 +320,7 @@ void FT8_end_cmd_burst(void)
 /* ---------------------- */
 
 
-/* Beginn a co-prozessor command */
+/* Begin a co-processor command */
 void FT8_start_cmd(uint32_t command)
 {
 	uint32_t ftAddress;
@@ -350,7 +362,7 @@ void FT8_cmd_dl(uint32_t command)
 }
 
 
-/* Write a string to co-processor memory in context of a command: no chip-select, just plain spi-transfers */
+/* Write a string to co-processor memory in context of a command: no chip-select, just plain SPI-transfers */
 void FT8_write_string(const char *text)
 {
 	uint8_t textindex = 0;
@@ -374,6 +386,7 @@ void FT8_write_string(const char *text)
 
 	FT8_inc_cmdoffset(textindex);
 }
+
 
 /* commands to draw graphics objects: */
 
@@ -1549,7 +1562,7 @@ void FT8_cmd_track(int16_t x0, int16_t y0, int16_t w0, int16_t h0, int16_t tag)
    which the results can be fetched after execution: FT8_memRead32(FT8_RAM_CMD + offset) */
 /* note: yes, these are different than the functions in the Programmers Guide from FTDI,
 	this is because I have no idea why anyone would want to pass "result" as an actual argument to these functions
-	when this only marks the offset the command-prozessor is writing to, it may even be okay to not transfer anything at all,
+	when this only marks the offset the command-processor is writing to, it may even be okay to not transfer anything at all,
 	just advance the offset by 4 bytes */
 
 /*
@@ -1802,11 +1815,50 @@ void FT8_cmd_rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t corne
 }
 
 
-/* init, has to be executed with the SPI setup to 11 MHz or less as required by FT8xx */
+/* FT811 / FT813 binary-blob from FTDIs AN_336 to patch the touch-engine for Goodix GT911 / GT9271 touch controllers */
+#ifdef FT8_HAS_GT911
 
+#if	defined (__AVR__)
+#include <avr/pgmspace.h>
+#else
+#define PROGMEM
+#endif
+
+const uint16_t FT8_GT911_len = 1184;
+const uint8_t FT8_GT911_data[1184] PROGMEM =
+{
+ 26,255,255,255,32,32,48,0,4,0,0,0,2,0,0,0,
+ 34,255,255,255,0,176,48,0,120,218,237,84,221,111,84,69,20,63,51,179,93,160,148,101,111,76,5,44,141,123,111,161,11,219,154,16,9,16,17,229,156,75,26,11,13,21,227,3,16,252,184,179,
+ 45,219,143,45,41,125,144,72,67,100,150,71,189,113,18,36,17,165,100,165,198,16,32,17,149,196,240,128,161,16,164,38,54,240,0,209,72,130,15,38,125,48,66,82,30,76,19,31,172,103,46,
+ 139,24,255,4,227,157,204,156,51,115,102,206,231,239,220,5,170,94,129,137,75,194,216,98,94,103,117,115,121,76,131,177,125,89,125,82,123,60,243,58,142,242,204,185,243,188,118,156,
+ 227,155,203,238,238,195,251,205,229,71,92,28,169,190,184,84,143,113,137,53,244,103,181,237,87,253,113,137,233,48,12,198,165,181,104,139,25,84,253,155,114,74,191,0,54,138,163,
+ 12,62,131,207,129,23,217,34,91,31,128,65,246,163,175,213,8,147,213,107,35,203,94,108,3,111,40,171,83,24,15,165,177,222,116,97,23,188,140,206,150,42,102,181,87,78,86,182,170,134,
+ 215,241,121,26,243,252,2,76,115,217,139,222,206,173,136,132,81,61,35,185,39,113,23,46,199,76,178,54,151,183,224,0,40,189,28,149,182,58,131,79,152,30,76,34,98,234,162,216,133,141,
+ 102,39,170,40,192,101,53,201,146,191,37,77,44,177,209,74,211,5,206,187,5,6,216,47,53,96,123,22,50,103,251,192,84,17,74,227,185,56,106,51,91,161,96,182,163,48,171,141,139,65,152,
+ 66,66,11,102,43,158,75,36,80,147,184,147,139,112,17,235,216,103,111,239,245,92,10,175,194,40,44,58,125,5,59,112,50,103,245,4,78,192,5,156,194,51,60,191,134,75,110,173,237,46,192,
+ 121,156,192,115,184,218,120,67,63,115,46,11,102,10,97,232,50,235,114,182,148,118,178,41,188,12,135,77,202,124,12,96,238,35,161,234,189,129,23,249,212,139,230,25,53,48,205,52,93,
+ 163,117,53,154,170,81,85,163,178,70,69,66,167,241,14,46,241,1,226,136,152,179,197,59,184,148,254,49,132,48,15,176,137,192,76,131,196,105,104,162,86,81,160,165,255,26,173,162,137,
+ 86,145,210,183,192,55,175,194,211,60,91,120,230,184,174,27,41,131,155,40,224,29,87,179,232,16,55,55,7,165,147,81,23,165,49,101,54,224,75,180,81,108,18,29,226,69,225,110,175,224,
+ 42,212,25,47,130,193,110,234,192,215,252,56,74,162,24,46,251,174,54,106,68,245,14,9,155,160,22,120,207,104,240,29,90,178,140,28,24,220,47,166,112,61,251,208,192,111,56,239,238,
+ 93,255,251,62,99,32,193,75,61,190,235,123,229,110,218,194,85,79,225,59,98,20,238,227,235,220,11,221,149,25,180,116,194,159,111,96,192,24,213,59,139,179,156,215,69,230,19,24,35,
+ 135,117,206,171,206,162,67,129,234,61,235,11,104,103,84,64,223,167,254,40,163,101,92,84,43,150,46,249,219,205,7,116,11,91,104,61,57,75,223,8,48,25,28,119,252,222,113,49,86,249,
+ 74,180,211,156,181,61,215,168,157,7,251,199,150,242,250,91,58,132,94,121,7,53,151,139,98,6,165,153,69,214,32,110,211,100,101,31,89,45,81,98,23,205,205,197,209,109,186,198,35,
+ 141,191,249,25,60,132,223,153,251,98,20,239,146,139,20,217,250,41,250,137,58,177,90,57,79,51,108,233,20,253,194,187,49,222,205,114,141,96,48,175,219,107,54,111,138,22,154,103,
+ 108,79,58,252,179,178,79,164,195,2,153,36,39,170,199,201,167,197,85,106,8,59,177,81,46,56,2,230,75,114,17,55,112,188,65,208,137,77,114,10,115,55,58,208,197,173,122,87,6,140,
+ 110,42,208,124,163,70,108,241,104,18,245,98,214,187,134,53,42,221,22,182,133,211,116,148,177,194,209,192,85,90,199,58,55,203,2,229,19,137,187,161,228,154,112,203,145,125,244,
+ 188,220,118,228,41,201,181,41,195,144,215,183,51,80,250,21,217,16,217,200,235,109,227,188,122,218,142,60,170,224,112,240,184,130,229,224,113,5,223,148,163,80,165,183,130,187,
+ 132,116,64,238,161,85,220,115,139,205,98,227,244,29,102,125,7,37,243,123,223,11,26,92,63,243,116,61,191,138,123,244,160,84,186,74,31,5,174,247,119,135,199,248,253,135,242,97,
+ 102,145,190,144,14,85,238,221,231,193,158,48,205,25,120,248,15,220,29,158,9,70,185,30,103,229,33,254,23,237,160,172,62,193,90,222,224,232,14,200,56,90,104,142,227,120,110,6,
+ 21,211,203,65,150,99,151,220,247,87,164,50,159,49,239,234,58,142,0,109,108,123,18,79,227,36,100,248,222,205,96,127,120,26,171,228,69,63,36,17,252,200,17,116,242,187,227,88,143,
+ 247,2,75,191,6,130,59,188,11,55,240,31,243,122,152,226,183,207,154,73,188,39,219,43,105,222,87,41,143,141,140,175,73,112,184,252,61,184,16,90,250,35,168,82,119,176,57,116,94,
+ 200,150,22,190,179,44,104,12,235,84,149,102,252,89,154,193,99,228,106,242,125,248,64,194,255,223,127,242,83,11,255,2,70,214,226,128,0,0
+};
+#endif
+
+
+/* init, has to be executed with the SPI setup to 11 MHz or less as required by FT8xx */
 uint8_t FT8_init(void)
 {
-	uint8_t gpio;
 	uint8_t chipid;
 	uint8_t timeout = 0;
 
@@ -1840,16 +1892,42 @@ uint8_t FT8_init(void)
 		}
 	}
 
-	FT8_memWrite8(REG_PCLK, 0x00);		/* set PCLK to zero - don't clock the LCD until later */
-	FT8_memWrite8(REG_PWM_DUTY, 10);		/* turn off backlight */
+/* we have a display with a Goodix GT911 / GT9271 touch-controller on it, so we patch our FT811 or FT813 according to AN_336 */
+#ifdef FT8_HAS_GT911
+	uint32_t ftAddress;
+
+	FT8_get_cmdoffset();
+	ftAddress = FT8_RAM_CMD + cmdOffset;
+
+	FT8_cs_set();
+	spi_transmit((uint8_t)(ftAddress >> 16) | MEM_WRITE); /* send Memory Write plus high address byte */
+	spi_transmit((uint8_t)(ftAddress >> 8)); /* send middle address byte */
+	spi_transmit((uint8_t)(ftAddress)); /* send low address byte */
+	spi_flash_write(FT8_GT911_data, FT8_GT911_len);
+	FT8_cs_clear();
+	FT8_cmd_execute();
+
+	FT8_memWrite8(REG_TOUCH_OVERSAMPLE, 0x0f); /* setup oversample to 0x0f as "hidden" in binary-blob for AN_336 */
+	FT8_memWrite16(REG_TOUCH_CONFIG, 0x05D0); /* write magic cookie as requested by AN_336 */
+
+	/* specific to the EVE2 modules from Matrix-Orbital we have to use GPIO3 to reset GT911 */
+	FT8_memWrite16(REG_GPIOX_DIR,0x8008); /* Reset-Value is 0x8000, adding 0x08 sets GPIO3 to output, default-value for REG_GPIOX is 0x8000 -> Low output on GPIO3 */
+	DELAY_MS(1); /* wait more than 100µs */
+	FT8_memWrite8(REG_CPURESET, 0x00); /* clear all resets */
+	DELAY_MS(56); /* wait more than 55ms */
+	FT8_memWrite16(REG_GPIOX_DIR,0x8000); /* setting GPIO3 back to input */
+#endif
+
+/*	FT8_memWrite8(REG_PCLK, 0x00);	*/	/* set PCLK to zero - don't clock the LCD until later, line disabled because zero is reset-default and we just did a reset */
+	FT8_memWrite8(REG_PWM_DUTY, 10);		/* turn off backlight, well, sort of, at least the modules from Glyn use inverted values */
 
 	/* Initialize Display */
-	FT8_memWrite16(REG_HSIZE,   FT8_HSIZE);	/* active display width */
+	FT8_memWrite16(REG_HSIZE,   FT8_HSIZE);		/* active display width */
 	FT8_memWrite16(REG_HCYCLE,  FT8_HCYCLE);	/* total number of clocks per line, incl front/back porch */
 	FT8_memWrite16(REG_HOFFSET, FT8_HOFFSET);	/* start of active line */
 	FT8_memWrite16(REG_HSYNC0,  FT8_HSYNC0);	/* start of horizontal sync pulse */
 	FT8_memWrite16(REG_HSYNC1,  FT8_HSYNC1);	/* end of horizontal sync pulse */
-	FT8_memWrite16(REG_VSIZE,   FT8_VSIZE);	/* active display height */
+	FT8_memWrite16(REG_VSIZE,   FT8_VSIZE);		/* active display height */
 	FT8_memWrite16(REG_VCYCLE,  FT8_VCYCLE);	/* total number of lines per screen, incl pre/post */
 	FT8_memWrite16(REG_VOFFSET, FT8_VOFFSET);	/* start of active screen */
 	FT8_memWrite16(REG_VSYNC0,  FT8_VSYNC0);	/* start of vertical sync pulse */
@@ -1860,38 +1938,28 @@ uint8_t FT8_init(void)
 	
 	/* Don't set PCLK yet - wait for just after the first display list */
 
-	/* Configure Touch */
+	/* configure Touch */
 	FT8_memWrite8(REG_TOUCH_MODE, FT8_TMODE_CONTINUOUS);	/* enable touch */
 	FT8_memWrite16(REG_TOUCH_RZTHRESH, FT8_TOUCH_RZTHRESH);	/* eliminate any false touches */
 
-	/* Configure Audio - not used, so disable it */
+	/* disable Audio for now */
 	FT8_memWrite8(REG_VOL_PB, 0x00);	/* turn recorded audio volume down */
-/*	FT8_memWrite8(REG_VOL_SOUND, 0xff); */	/* turn synthesizer volume on */
 	FT8_memWrite8(REG_VOL_SOUND, 0x00);	/* turn synthesizer volume off */
 	FT8_memWrite16(REG_SOUND, 0x6000);	/*	set synthesizer to mute */
 
+	/* write a basic display-list to get things started */
 	FT8_memWrite32(FT8_RAM_DL, DL_CLEAR_RGB);
 	FT8_memWrite32(FT8_RAM_DL + 4, (DL_CLEAR | CLR_COL | CLR_STN | CLR_TAG));
 	FT8_memWrite32(FT8_RAM_DL + 8, DL_DISPLAY);	/* end of display list */
 	FT8_memWrite32(REG_DLSWAP, FT8_DLSWAP_FRAME);
 
 	/* nothing is being displayed yet... the pixel clock is still 0x00 */
+	FT8_memWrite8(REG_GPIO, 0x80); /* enable the DISP signal to the LCD panel, it is set to output in REG_GPIO_DIR by default */
+	FT8_memWrite8(REG_PCLK, FT8_PCLK); /* now start clocking data to the LCD panel */
+	FT8_memWrite8(REG_PWM_DUTY, 70); /* turn on backlight to some value that needs to be adjusted or not... */
 
-	gpio = FT8_memRead8(REG_GPIO_DIR);
-	gpio |= 0x82; /* set DISP to Output although it always is output, set GPIO1 to Output - Audio Enable on VM800B */
-	FT8_memWrite8(REG_GPIO_DIR, gpio);
-
-	gpio = FT8_memRead8(REG_GPIO);	/* read the FT8xx GPIO register for a read/modify/write operation */
-/*	gpio |= 0x82; */					/* set bit 7 of FT8xx GPIO register (DISP), set GPIO1 to High to enable Audio - others are inputs */
-	gpio |= 0x80;						/* set bit 7 of FT8xx GPIO register (DISP), others are inputs */
-	FT8_memWrite8(REG_GPIO, gpio);	/* enable the DISP signal to the LCD panel */
-	FT8_memWrite8(REG_PCLK, FT8_PCLK);	/* now start clocking data to the LCD panel */
-
-	FT8_memWrite8(REG_PWM_DUTY, 70);	/* turn on backlight */
-
-	DELAY_MS(2);	/* just to be safe */
-	
-	while(FT8_busy() == 1);
-	FT8_get_cmdoffset();
+	DELAY_MS(2); /* just to be safe */
+	while(FT8_busy() == 1); /* just to be safe, should return immediately */
+	FT8_get_cmdoffset(); /* just to be safe */
 	return 1;
 }
