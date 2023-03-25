bf6d1d070bf4d001a7a3a2d9fd54c147ece7f618
RudolphRiedel <31180093+RudolphRiedel@users.noreply.github.com>
2023-03-25 22:23:44 +0100

    - optimized the binary data for the basic example

--- examples/EVE_Test_SAMC21_EVE2-50G/tft_data.h
+++ examples/EVE_Test_SAMC21_EVE2-50G/tft_data.h
@@ -3,15 +3,15 @@
 #define TFT_DATA_H
 
 #if	defined (__AVR__)
-	#include <avr/pgmspace.h>
+    #include <avr/pgmspace.h>
 #else
-	#if !defined(PROGMEM)
-		#define PROGMEM
-	#endif
+    #if !defined(PROGMEM)
+        #define PROGMEM
+    #endif
 #endif
 
-extern const uint8_t logo[239] PROGMEM;
-extern const uint8_t pic[3844] PROGMEM;
-extern const uint8_t flash[8199] PROGMEM;
+extern const uint8_t logo[206] PROGMEM;
+extern const uint8_t pic[3391] PROGMEM;
+extern const uint8_t flash[7765] PROGMEM;
 
 #endif /* TFT_DATA_H */
