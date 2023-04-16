6e5924dd32074d6877f46d48adc650aa03b8f863
RudolphRiedel <31180093+RudolphRiedel@users.noreply.github.com>
2023-04-16 12:17:13 +0200

    - fixed the ESP32 example to work with ESP-IDF 5 and expanded it with a config for ESP32-S3

--- examples/EVE_Test_ESP32_PlatformIO/src/main.c
+++ examples/EVE_Test_ESP32_PlatformIO/src/main.c
@@ -2,35 +2,26 @@
 @file    main.c
 @brief   Main file for ESP-IDF ESP32 example
 @version 1.0
-@date    2021-01-08
+@date    2023-04-16
 @author  Rudolph Riedel
 */
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#include "soc/rtc_wdt.h"
 
-#include "EVE_target.h"
-#include "EVE_commands.h"
+#if ESP_IDF_VERSION_MAJOR == 5
+#include "esp_timer.h"
+#endif
+
+#include "EVE.h"
 #include "tft.h"
-
-#define LED_BUILTIN GPIO_NUM_2
 
 void app_main()
 {
     uint32_t current_millis;
     uint32_t previous_millis = 0;
     uint8_t display_delay = 0;
-    uint16_t led = 0;
-    uint8_t led_state = 0;
     uint32_t micros_start, micros_end;
-    gpio_config_t io_cfg = {0};
-
-    io_cfg.intr_type = GPIO_PIN_INTR_DISABLE;
-    io_cfg.mode = GPIO_MODE_OUTPUT;
-    io_cfg.pin_bit_mask = BIT(LED_BUILTIN);
-    gpio_config(&io_cfg);
 
     EVE_init_spi();
     TFT_init();
@@ -42,22 +33,6 @@ void app_main()
         if((current_millis - previous_millis) > 4) /* execute the code every 5 milli-seconds */
         {
             previous_millis = current_millis;
-
-            led++;
-            if(led > 69)
-            {
-                led = 0;
-                if(led_state)
-                {
-                    led_state = 0;
-                    gpio_set_level(LED_BUILTIN, 0);
-                }
-                else
-                {
-                    led_state = 42;
-                    gpio_set_level(LED_BUILTIN, 1);
-                }
-            }
 
             micros_start = (uint32_t) (esp_timer_get_time());
             TFT_touch();
@@ -74,7 +49,7 @@ void app_main()
                 num_profile_a = (micros_end - micros_start); /* calculate the micro-seconds passed during the call to TFT_display */
             }
         }
-    rtc_wdt_feed();
+
     vPortYield();
     }
 }
