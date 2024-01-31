8face38b9831c4ac0e966f4c5c7e697229c18d97
RudolphRiedel <31180093+RudolphRiedel@users.noreply.github.com>
2024-01-31 18:35:36 +0100

    - example configuration file than be included into EVE_config.h by defining the macro EVE_CUSTOM_MODULE_H

---
+++ examples/mycfg.h
@@ -0,0 +1,23 @@
+/*
+@file    mycfg.h
+@brief   example custom module configuration header file
+@version 1.0
+@date    2024-01-31
+@author  Rudolph Riedel
+
+*/
+
+#ifndef MYCFG_H
+#define MYCFG_H
+
+#define Resolution_800x480
+
+#define EVE_PCLK (2L)
+#define EVE_PCLKPOL (1L)
+#define EVE_SWIZZLE (0L)
+#define EVE_CSPREAD (0L)
+#define EVE_HAS_CRYSTAL
+#define EVE_GEN 3
+#define EVE_HAS_GT911
+
+#endif /* MYCFG_H */
